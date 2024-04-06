4101491a3e7fe1f2095e6b9deff902482b1ab1a2
Vaqint <samuelcollette23@hotmail.com>
2024-04-06 18:59:01 -0400

    entity linking working

--- automateFileInfo.py
+++ automateFileInfo.py
@@ -1,2 +1,12 @@
 import spacy
+import en_core_web_sm
 
+text = open("./COMP474/Worksheets/worksheet01.txt", "r", encoding="utf8")
+readText = text.read()
+nlp = spacy.load('en_core_web_sm')
+nlp.add_pipe('entityfishing')
+doc = nlp(readText)
+for span in doc.ents:
+    if (span.label_ != 'CARDINAL' and span.label_ != 'ORDINAL' and span.label_ != 'PRODUCT' and span.label_ != 'LOC'):
+        print((span.text, span.label_, span._.kb_qid, span._.url_wikidata, span._.nerd_score))
+    
