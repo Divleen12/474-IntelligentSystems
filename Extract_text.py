e35fb55824abf99c3b83773af666bb0d89494e26
QueenKimm <gudarzikimia@gmail.com>
2024-04-06 17:47:52 -0400

    Concordia university info nt file

--- Extract_text.py
+++ Extract_text.py
@@ -15,42 +15,42 @@ def SaveFile(text, output_path):
 for i in range(1, 8):
     SlideNum = str(i).zfill(2)
     pdf_path = f'/Users/kimiagoodarzi/Desktop/roboprof/COMP4746741/Lectures/slides{SlideNum}.pdf'
-    output_file_path = f'COMP474/Slides/Slide_{SlideNum}.txt'
+    outputPath = f'COMP474/Slides/Slide_{SlideNum}.txt'
     textContent = ExtractText(pdf_path)
-    SaveFile(textContent, output_file_path)
+    SaveFile(textContent, outputPath)
     print(f'Extracted and saved slide{SlideNum}.pdf')
 
 
 for i in range(1, 7):
     worksheetNum = str(i).zfill(2)
     pdf_path = f'/Users/kimiagoodarzi/Desktop/roboprof/COMP4746741/Worksheets/Worksheet{worksheetNum}.pdf'
-    output_file_path = f'COMP474/Worksheets/worksheet{worksheetNum}.txt'
-    text_content = ExtractText(pdf_path)
-    SaveFile(text_content, output_file_path)
+    outputPath = f'COMP474/Worksheets/worksheet{worksheetNum}.txt'
+    TextContent = ExtractText(pdf_path)
+    SaveFile(TextContent, outputPath)
     print(f'Extracted and saved text from Worksheet{worksheetNum}.pdf')
 
 
 pdf_path = '/Users/kimiagoodarzi/Desktop/roboprof/COMP4746741/course_outline_comp474_6741_w2024.pdf'
-output_file_path = 'COMP474/course_outline_comp474_6741_w2024.txt'
-text_content = ExtractText(pdf_path)
-SaveFile(text_content, output_file_path)
+outputPath = 'COMP474/course_outline_comp474_6741_w2024.txt'
+TextContent = ExtractText(pdf_path)
+SaveFile(TextContent, outputPath)
 
 
 for i in range(1, 8):
     SlideNum = str(i).zfill(2)
     pdf_path = f'/Users/kimiagoodarzi/Desktop/roboprof/COMP472/Lectures/slides{SlideNum}.pdf'
-    output_file_path = f'COMP472/Slides/Slide_{SlideNum}.txt'
+    outputPath = f'COMP472/Slides/Slide_{SlideNum}.txt'
     textContent = ExtractText(pdf_path)
-    SaveFile(textContent, output_file_path)
+    SaveFile(textContent, outputPath)
 
 for i in range(1, 7):
     worksheetNum = str(i).zfill(2)
     pdf_path = f'/Users/kimiagoodarzi/Desktop/roboprof/COMP472/Worksheets/Worksheet{worksheetNum}.pdf'
-    output_file_path = f'COMP472/Worksheets/worksheet{worksheetNum}.txt'
-    text_content = ExtractText(pdf_path)
-    SaveFile(text_content, output_file_path)
+    outputPath = f'COMP472/Worksheets/worksheet{worksheetNum}.txt'
+    TextContent = ExtractText(pdf_path)
+    SaveFile(TextContent, outputPath)
 
 pdf_path = '/Users/kimiagoodarzi/Desktop/roboprof/COMP472/course_outline_comp472_w2024.pdf'
-output_file_path = 'COMP472/course_outline_comp472_w2024.txt'
-text_content = ExtractText(pdf_path)
-SaveFile(text_content, output_file_path)
+outputPath = 'COMP472/course_outline_comp472_w2024.txt'
+TextContent = ExtractText(pdf_path)
+SaveFile(TextContent, outputPath)
