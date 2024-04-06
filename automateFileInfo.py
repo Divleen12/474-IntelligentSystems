73ddc2c4763da3def9f77032b6d6ab86f364673f
Vaqint <samuelcollette23@hotmail.com>
2024-04-06 19:07:03 -0400

    write to file for linked entities

--- automateFileInfo.py
+++ automateFileInfo.py
@@ -6,7 +6,11 @@ readText = text.read()
 nlp = spacy.load('en_core_web_sm')
 nlp.add_pipe('entityfishing')
 doc = nlp(readText)
+
+outputPath = f'COMP474/Worksheets/worksheet01_LinkedEntities.txt'
+
+file = open(outputPath, "w", encoding = "utf8")
+
 for span in doc.ents:
     if (span.label_ != 'CARDINAL' and span.label_ != 'ORDINAL' and span.label_ != 'PRODUCT' and span.label_ != 'LOC'):
-        print((span.text, span.label_, span._.kb_qid, span._.url_wikidata, span._.nerd_score))
-    
+        file.write(str((span.text, span.label_, span._.kb_qid, span._.url_wikidata, span._.nerd_score)) + "\n")
